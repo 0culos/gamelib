735450dd06eea38e2445250de1fb2b120dfabf9f
danilo pereira <danilo.pereira@multpex.com.br>
2023-07-18 18:35:29 -0300

    added create view in gamelib

--- gamelib.py
+++ gamelib.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 class Game:
@@ -11,19 +11,38 @@ class Game:
         return f"{self.name}, {self.category}, {self.console}"
 
 
+game1 = Game('Tetris', 'Puzzle', 'Atari')
+game2 = Game('God of War', 'Rack and Slash', 'PS2')
+game3 = Game('Crash', 'Adventure', 'PS1')
+game_list = [game1, game2, game3]
+
 app = Flask(__name__)
 
 
-@app.route('/start')
-def hello():
-    game1 = Game('Tetris', 'Puzzle', 'Atari')
-    game2 = Game('God of War', 'Rack and Slash', 'PS2')
-    game3 = Game('Crash', 'Adventure', 'PS1')
-    game_list = [game1, game2, game3]
+@app.route('/')
+def index():
+
     return render_template('list.html', title='Games', games=game_list)
 
 
 @app.route('/new_game')
 def new_game():
     return render_template('new_game.html', title='New Game')
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        category = request.form['category']
+        console = request.form['console']
+
+        game = Game(name, category, console)
+        game_list.append(game)
+
+        return redirect(url_for('index'))
+
+    return render_template('list.html', title='Games', games=game_list)
+
+
 app.run()
